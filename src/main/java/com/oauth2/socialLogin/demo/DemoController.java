b78bf4dd55722299702d973be19b4fda7b85b439
Ritesh Jaiswal <riteshjaizz@gmail.com>
2023-09-21 18:59:17 +0530

    Implemented OAuth 2.0

---
+++ src/main/java/com/oauth2/socialLogin/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.oauth2.socialLogin.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> demoFunction() {
+        return new ResponseEntity<>("Hello From Demo Controller", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/oauth2/socialLogin/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.socialLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
